f665d6e5dae5a71fe3618920af63ba6b129334fc
Mehedi Hasan Sizar <78275602+mhsizar@users.noreply.github.com>
2023-05-24 19:44:37 -0400

    Update hide_unhide.rs

--- src/hide_unhide.rs
+++ src/hide_unhide.rs
@@ -3,17 +3,22 @@ use std::os::windows::{fs::MetadataExt, ffi::OsStrExt};
 use winapi::um::winnt::FILE_ATTRIBUTE_HIDDEN;
 
 pub fn hide_directory(directory: &Path) -> Result<(), Error> {
-    let metadata = directory.metadata()?;
+    let metadata = directory.metadata()?; // Get the the metadata of the directory provided
+    
+    // Set the permission of the directory, returns error if directory doesn't exist——useful to evaluate the user input
     let mut permissions = metadata.permissions();
     permissions.set_readonly(true);
     std::fs::set_permissions(directory, permissions)?;
     
+    // Check the file attributes from the metadata of the directory provided and see if it's already hidden
     let is_hidden = metadata.file_attributes() & FILE_ATTRIBUTE_HIDDEN != 0;
 
     if is_hidden {
         println!("Directory is already hidden!");
     } else {
-        let file_attributes = metadata.file_attributes() | FILE_ATTRIBUTE_HIDDEN;
+        let file_attributes = metadata.file_attributes() | FILE_ATTRIBUTE_HIDDEN; // set attribute to FILE_ATTRIBUTE_HIDDEN if it's not already
+    
+        // Update the directory attribute to hide it (unsafe behaviour)
         unsafe {
         winapi::um::fileapi::SetFileAttributesW(directory
             .as_os_str()
@@ -29,17 +34,22 @@ pub fn hide_directory(directory: &Path) -> Result<(), Error> {
 }
 
 pub fn unhide_directory(directory: &Path) -> Result<(), Error> {
-    let metadata = directory.metadata()?;
+    let metadata = directory.metadata()?; // Get the the metadata of the directory provided
+    
+    // Set the permission of the directory, returns error if directory doesn't exist——useful to evaluate the user input
     let mut permissions = metadata.permissions();
     permissions.set_readonly(false);
     std::fs::set_permissions(directory, permissions)?;
 
+    // Get the file attributes from the metadata of the directory provided and see if it's already hidden
     let is_hidden = metadata.file_attributes() & FILE_ATTRIBUTE_HIDDEN != 0;
 
     if !is_hidden {
         println!("Directory is already unhidden!");
     } else {
-        let file_attributes = metadata.file_attributes() & !FILE_ATTRIBUTE_HIDDEN;
+        let file_attributes = metadata.file_attributes() & !FILE_ATTRIBUTE_HIDDEN; // set attribute to not FILE_ATTRIBUTE_HIDDEN to unhide it
+    
+        // Update the directory attribute to unhide it (unsafe behaviour)
         unsafe {
             winapi::um::fileapi::SetFileAttributesW(directory
                 .as_os_str()
