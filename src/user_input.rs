d9c58f2c3d5ccde6c77c6f2bf1582f1369df56e0
Mehedi Hasan Sizar <78275602+mhsizar@users.noreply.github.com>
2023-05-24 19:35:15 -0400

    Update user_input.rs

--- src/user_input.rs
+++ src/user_input.rs
@@ -2,6 +2,7 @@ use std::io::Write;
 use std::path::{Path, PathBuf};
 
 pub fn get_operation_from_user() -> String {
+    // Ask user to input hide/unhide/exit to perform appropriate operation
     loop {
         let mut operation = String::new();
         println!("Enter 'hide' or 'unhide': ");
@@ -19,16 +20,18 @@ pub fn get_operation_from_user() -> String {
 
 
 pub fn get_directory_from_user() -> PathBuf {
+    // Loop until user inputs a valid directory path
     loop {
         let mut directory = String::new();
         print!("Enter directory path: ");
         std::io::stdout().flush().unwrap();
         std::io::stdin().read_line(&mut directory).expect("Failed to read line");
 
-        let directory = directory.trim().trim_matches('"'); // remove leading/trailing whitespace and quotes
+        let directory = directory.trim().trim_matches('"'); // Remove leading/trailing whitespace and quotes
 
-        let file_path = Path::new(directory);
-
+        let file_path = Path::new(directory); // Inspect the path from the directory provided
+        
+        // Check if the path exists, return the owned path if it is a directory
         if !file_path.exists() {
             println!("Directory '{}' does not exist. Please try again.", directory);
             continue;
@@ -36,7 +39,7 @@ pub fn get_directory_from_user() -> PathBuf {
             println!("'{}' is not a directory. Please try again.", directory);
             continue;
         } else {
-            return file_path.to_path_buf();
+            return file_path.to_path_buf(); // Convert the Path to an owned PathBuf.
         }
     }
 }
